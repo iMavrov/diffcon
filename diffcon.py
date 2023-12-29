b1ca08795523ed747c55c305ac1d14b90975eefa
Ivan Mavrov <ivan@alcatraz.ai>
2023-12-29 21:46:09 +0200

    Tidy the code a bit and dump it into a JSON

--- diffcon.py
+++ diffcon.py
@@ -1,11 +1,46 @@
-import os
-import json
 from collections import deque
 import hashlib
+import json
+import os
+import time
 
-snapshot = []
 
-def calculate_checksum(file_path):
+class Snapshot:
+
+  def __init__(self):
+      self.snapshot = {}
+
+  def add(self, root, skip_directories=[]):
+      root = os.path.abspath(root)
+      if not os.path.exists(root) or not os.path.isdir(root):
+          return
+      
+      directories = deque([root])
+      while directories:
+        directory = directories.popleft()
+        if directory in skip_directories:
+          continue
+
+        with os.scandir(directory) as entries:
+          for entry in entries:
+              if entry.is_dir(follow_symlinks=False):
+                  directories.append(entry.path)
+              elif entry.is_file(follow_symlinks=False):
+                self.handle_file(entry)
+
+  def dump(self):
+    timestamp = int(time.time())
+    with open('snapshot-{}.json'.format(timestamp), "w") as file:
+      json.dump(self.snapshot, file)
+
+  def handle_file(self, file):
+    metadata = file.stat(follow_symlinks=False)
+    last_modified = metadata.st_mtime
+    size = metadata.st_size
+    checksum = self.calculate_checksum(file.path)
+    self.snapshot[file.path] = (last_modified, size, checksum)
+
+  def calculate_checksum(self, file_path):
     sha256 = hashlib.sha256()
     with open(file_path, 'rb') as file:
         for chunk in iter(lambda: file.read(4096), b''):
@@ -13,39 +48,14 @@ def calculate_checksum(file_path):
     return sha256.hexdigest()
 
 
-def handle_file(file):
-  metadata = file.stat(follow_symlinks=False)
-  last_modified = metadata.st_mtime
-
-  checksum = calculate_checksum(file.path)
-
-  snapshot.append((file.path, last_modified, checksum))
-
-
-def traverse(root, skip_directories=[]):
-    root = os.path.abspath(root)
-    if not os.path.exists(root) or not os.path.isdir(root):
-        return
-    
-    directories = deque([root])
-    while directories:
-      directory = directories.popleft()
-      if directory in skip_directories:
-         continue  
-      with os.scandir(directory) as entries:
-        for entry in entries:
-            if entry.is_dir(follow_symlinks=False):
-                directories.append(entry.path)
-            elif entry.is_file(follow_symlinks=False):
-              handle_file(entry)
-
-
 def main():
-    # config = json.load("config.json")
-    root = "/home/mavrov/Downloads"
-    traverse(root)
-    print(snapshot)
+    snapshot = Snapshot()
+    with open('config.json') as file:
+      config = json.load(file)
+      for root in config['roots']:
+         snapshot.add(root['path'], root['skip'] if 'skip' in root else [])
+    snapshot.dump()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
